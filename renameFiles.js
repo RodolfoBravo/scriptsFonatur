81dc1562e62cbc69dc6f0effcac59ea55e643b79
RodolfoBravo <rodolfobravogarcia@gmail.com>
2023-07-29 21:33:30 -0600

    update Script1.5

--- renameFiles.js
+++ renameFiles.js
@@ -3,27 +3,42 @@ const path = require("path");
 
 const directorio = "/home/rodolfobravogarcia/fonatur-backend/uploads/etapa2"; // Reemplaza con el path del directorio que deseas analizar
 
-fs.readdir(directorio, (err, archivos) => {
-  if (err) {
-    console.error("Error al leer el directorio:", err);
-    return;
-  }
-  console.log(archivos);
-  archivos.forEach((archivo) => {
-    if (archivo.includes(".pdf.pdf")) {
-      console.log(archivo);
-      //const nuevoNombre = archivo.replace(".pdf.pdf", ".pdf");
-      /* const rutaAnterior = path.join(directorio, archivo);
-      const rutaNueva = path.join(directorio, nuevoNombre);
+function renombrarArchivos(directorio) {
+  fs.readdir(directorio, (err, archivos) => {
+    if (err) {
+      console.error("Error al leer el directorio:", err);
+      return;
+    }
 
-      fs.rename(rutaAnterior, rutaNueva, (error) => {
+    archivos.forEach((archivo) => {
+      const rutaArchivo = path.join(directorio, archivo);
+      fs.stat(rutaArchivo, (error, stats) => {
         if (error) {
-          console.error(`Error al renombrar el archivo ${archivo}:`, error);
-        } else {
-          console.log(`Archivo ${archivo} renombrado como ${nuevoNombre}`);
+          console.error("Error al obtener información del archivo:", error);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          // Si es un directorio, recorre sus archivos también
+          renombrarArchivos(rutaArchivo);
+        } else if (archivo.includes(".pdf.pdf")) {
+          console.log(archivo);
+          /*
+            const nuevoNombre = archivo.replace('.pdf.pdf', '.pdf');
+            const rutaNueva = path.join(directorio, nuevoNombre);
+  
+            fs.rename(rutaArchivo, rutaNueva, (renameError) => {
+              if (renameError) {
+                console.error(`Error al renombrar el archivo ${archivo}:`, renameError);
+              } else {
+                console.log(`Archivo ${archivo} renombrado como ${nuevoNombre}`);
+              }
+            });*/
         }
       });
-      */
-    }
+    });
   });
-});
+}
+
+// Inicia el proceso de renombrar archivos desde el directorio raíz
+renombrarArchivos(directorio);
